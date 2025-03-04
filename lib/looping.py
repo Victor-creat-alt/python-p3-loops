acd4fcb890d38735cc906d703e3e3f2f3921325e
Victor <victor.kihiko@student.moringaschool.com>
2025-03-04 19:03:34 +0300

    modified looping.py

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+        print("Happy New Year!")      
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [list**2 for list in int_list]
+pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
